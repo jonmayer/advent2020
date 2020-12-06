f760f8c819c935950006e2faae26e9aba8953d83
Jonathan Mayer <jonathan.mayer@gmail.com>
2020-12-06 09:06:57 -0800

    removed duplication

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -3,31 +3,31 @@ use advent;
 const SURVEY_SIZE: usize = 26;
 
 struct SurveySet {
-    answer_count: [u32; SURVEY_SIZE],  // number of yes answers for 26 questions.
-    num_of_records: u32
+    yes_count: [u32; SURVEY_SIZE],  // number of yes answers for 26 questions.
+    answer_count: u32
 }
 
 impl SurveySet {
     fn new() -> SurveySet {
-        SurveySet { answer_count: [0; SURVEY_SIZE], num_of_records: 0 }
+        SurveySet { yes_count: [0; SURVEY_SIZE], answer_count: 0 }
     }
 
     fn read_record(&mut self, record: &str) {
         for c in record.chars() {
-            if c == '\n' { self.num_of_records += 1; }
+            if c == '\n' { self.answer_count += 1; }
             let index = (c as usize).wrapping_sub('a' as usize);
-            if index <= SURVEY_SIZE {self.answer_count[index] += 1;}
+            if index <= SURVEY_SIZE {self.yes_count[index] += 1;}
         }
-        self.num_of_records += 1;
+        self.answer_count += 1;
     }
 
     fn count_nonzero_answers(&self) -> u32 {
-        return self.answer_count.iter().filter(|&&x| x > 0).count() as u32;
+        return self.yes_count.iter().filter(|&&x| x > 0).count() as u32;
     }
 
     fn count_allyes_answers(&self) -> u32 {
-        return self.answer_count.iter()
-            .filter(|&&x| x == self.num_of_records).count() as u32;
+        return self.yes_count.iter()
+            .filter(|&&x| x == self.answer_count).count() as u32;
     }
 }
 
@@ -36,25 +36,16 @@ fn main() {
 
     let records: Vec<&str> = content.split("\n\n").collect();
 
-    {
-        println!("part 1");
-        let mut total = 0;
-        for r in records.iter() {
-            let mut ss: SurveySet = SurveySet::new();
-            ss.read_record(r);
-            total += dbg!(ss.count_nonzero_answers());
-        }
-        dbg!(total);
+    let mut part1_total = 0;
+    let mut part2_total = 0;
+
+    for r in records.iter() {
+        let mut ss: SurveySet = SurveySet::new();
+        ss.read_record(r);
+        part1_total += dbg!(ss.count_nonzero_answers());
+        part2_total += dbg!(ss.count_allyes_answers());
     }
 
-    {
-        println!("part 2");
-        let mut total = 0;
-        for r in records.iter() {
-            let mut ss: SurveySet = SurveySet::new();
-            ss.read_record(r);
-            total += dbg!(ss.count_allyes_answers());
-        }
-        dbg!(total);
-    }
+    dbg!(part1_total);
+    dbg!(part2_total);
 }
