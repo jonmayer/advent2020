e68aebf7b8c68dc2477affb61bc3984336e420ea
Jonathan Mayer <jonathan.mayer@gmail.com>
2020-12-06 08:26:56 -0800

    better variable names.

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -4,21 +4,21 @@ const SURVEY_SIZE: usize = 26;
 
 struct SurveySet {
     answer_count: [u32; SURVEY_SIZE],  // number of yes answers for 26 questions.
-    entries: u32
+    num_of_records: u32
 }
 
 impl SurveySet {
     fn new() -> SurveySet {
-        SurveySet { answer_count: [0; SURVEY_SIZE], entries: 0 }
+        SurveySet { answer_count: [0; SURVEY_SIZE], num_of_records: 0 }
     }
 
     fn read_record(&mut self, record: &str) {
         for c in record.chars() {
-            if c == '\n' { self.entries += 1; }
+            if c == '\n' { self.num_of_records += 1; }
             let index = (c as usize).wrapping_sub('a' as usize);
             if index <= SURVEY_SIZE {self.answer_count[index] += 1;}
         }
-        self.entries += 1;
+        self.num_of_records += 1;
     }
 
     fn count_nonzero_answers(&self) -> u32 {
@@ -27,7 +27,7 @@ impl SurveySet {
 
     fn count_allyes_answers(&self) -> u32 {
         return self.answer_count.iter()
-            .filter(|&&x| x == self.entries).count() as u32;
+            .filter(|&&x| x == self.num_of_records).count() as u32;
     }
 }
 
