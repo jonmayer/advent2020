d1e1f641dfdce56016a66e36c118804ce979a8e0
Jonathan Mayer <jonathan.mayer@gmail.com>
2020-12-12 09:03:20 -0800

    use iter product instead of loop

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -6,6 +6,36 @@ struct Adapters {
     sequence: Vec<i64>,
 }
 
+// Takes a length of delta-1 sequence and returns the number of legal
+// mutations of that sequence (including the original).
+fn length_to_mutations(l: usize) -> u128 {
+        // All 1-delta subsequences are isomorphic with the following examples.
+        // The first joltage and the final joltage are fixed, but some of the
+        // steps in between can be removed as long as no joltage jump is greater
+        // than 3.
+        //
+        // length 1: 0 1  --> 1 mutation
+        // length 2: 0 1 2
+        //           0   2  --> 2 mutations (the 1 can be dropped)
+        // length 3: 0 1 2 3
+        //           0   2 3
+        //           0 1   3
+        //           0     3 --> 4 mutations
+        // length 4: 0 1 2 3 4
+        //           0   2 3 4
+        //           0 1   3 4
+        //           0 1 2   4
+        //           0   2   4
+        //           0     3 4
+        //           0 1     4 -> 7 mutations
+        return match l {
+                1 => 1u128,
+                2 => 2u128,
+                3 => 4u128,
+                4 => 7u128,
+                _ => 0u128 };
+}
+
 impl Adapters {
     fn new() -> Adapters {
         Adapters { joltage: Vec::new(), sequence: Vec::new() }
@@ -37,7 +67,6 @@ impl Adapters {
     fn count_subsequences(&self) -> Vec<usize> {
         let mut lengths: Vec<usize> = Vec::new();
         let mut count = 0usize;
-        let mut j = 0;
         for i in 1..self.sequence.len() {
             let delta = (self.sequence[i] - self.sequence[i-1]) as usize;
             if delta == 1 { count += 1 }
@@ -57,37 +86,7 @@ impl Adapters {
     fn count_legal_combos(&self) -> u128 {
         // count up the various 1-delta subsequences.
         let lengths: Vec<usize> = self.count_subsequences();
-        // All 1-delta subsequences are isomorphic with the following examples.
-        // The first joltage and the final joltage are fixed, but some of the
-        // steps in between can be removed as long as no joltage jump is greater
-        // than 3.
-        //
-        // length 1: 0 1  --> 1 permutation in permutation set.
-        // length 2: 0 1 2
-        //           0   2  --> 2 permutations (the 1 can be dropped)
-        // length 3: 0 1 2 3
-        //           0   2 3
-        //           0 1   3
-        //           0     3 --> 4 permutations
-        // length 4: 0 1 2 3 4
-        //           0   2 3 4
-        //           0 1   3 4
-        //           0 1 2   4
-        //           0   2   4
-        //           0     3 4
-        //           0 1     4 -> 7 permutations
-        let mut product = 1u128;
-        for l in lengths.iter() {
-            let combos = match l {
-                1 => 1u128,
-                2 => 2u128,
-                3 => 4u128,
-                4 => 7u128,
-                _ => 0u128 };
-            product *= combos;
-            dbg!(product);
-        }
-        return product;
+        return lengths.iter().map(|x| length_to_mutations(*x)).product();
     }
 }
 
@@ -101,6 +100,7 @@ fn main() {
 
     dbg!(adapters.count_subsequences());
     let part2 = adapters.count_legal_combos();
+    assert_eq!(3454189699072, part2);  // now that I know the answer...
 
     dbg!(part1);
     dbg!(part2);
