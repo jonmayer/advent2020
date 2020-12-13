4d00aa24f9cc7fdc6b7598d191fa2948c01f8c80
Jonathan Mayer <jonathan.mayer@gmail.com>
2020-12-13 11:33:13 -0800

    cleaned up comments.

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -38,7 +38,8 @@ fn all_equal(x: &Vec<i128>) -> bool {
     return true;
 }
 
-// A brute force implementation for solving the system of equations.
+// A brute force implementation for solving the system of equations.  This approach is predictably
+// too slow for this problem, but I wanted something reliable to regress my fancier design against.
 //
 // k(i) * ids(i) - offsets(i) = k(j) * ids(j) - offsets(j)
 // for all i,j
@@ -71,63 +72,36 @@ fn bf_converge_vector(ids: Vec<i128>, offsets: Vec<i128>) -> i128 {
     return k[0] * ids[0];
 }
 
-fn part2(text: &str) -> i128 {
-    let v: Vec<_> = text.split(",")
-        .enumerate()
-        .filter(|(_, id)| *id != "x")
-        .collect();
-    dbg!(&v);
-    let buses: Vec<Bus> = v.iter()
-        .map(|(i, id)|
-             Bus { interval: id.parse::<i128>().unwrap(),
-                   launch: (*i as i128) * -1, })
-        .collect();
-    dbg!(&buses);
-    let mut bus: Bus = buses[0].clone();
-    for i in 1..buses.len() {
-        bus.combine_with_other_bus(&buses[i]);
-        dbg!(&bus);
-    }
-    return bus.launch;
-}
-
-fn main() {
-    let content = advent::load_input();
-
-    dbg!(part1);
-
-    // brute force test of part2 solving
-    let v: Vec<i128> = vec![3i128, 5, 7, 11];
-    let o: Vec<i128> = vec![0i128, 1, 2, 3];
-    dbg!(bf_converge_vector(v, o));
-
-    // elegant faster solution over the same data:
-    dbg!(part2(&"3,5,7,11"));
-  
-    // the real solution to the input data set:
-    println!("the real answer");
-    dbg!(part2(&lines[1]));
-}
+// PART 2 - the fancy approach
+//
+// Instead of thinking of the buses as having staggered arrival times, I decided that it was
+// conceptually easier to give the buses staggered start times, and solve for a timestamp
+// where they would all arrive simultaneously.
+//
+// This approach takes a pair of buses and finds the first time those buses intersect, and
+// the interval with which they intersection will repeat.  From that, we can synthesize
+// a new bus with a new interval and start_time (the time of the first intersection).
+//
+// By combining one bus at a time into our synthetic super-bus, we can iterate through
+// the vector of buses and find the timestamp where all buses show up at the same time.
+// This is isomorphic with the buses showing up on a staggered schedule, since we moved
+// the staggering offset to be staggered start_times instead.
 
 #[derive(Clone, Debug)]
 struct Bus {
     interval: i128,
-    launch: i128,
+    start_time: i128,
 }
 
 impl Bus {
-    fn new(i: i128, n: i128) -> Bus {
-        Bus {
-            interval: i,
-            launch: n,
-        }
-    }
-
+    // Compute the kth arrival.
     fn arrival_time(&self, k: i128) -> i128 {
-        return k * self.interval + self.launch;
+        return k * self.interval + self.start_time;
     }
 
+    // Make a super bus by combining this bus with another bus.
     fn combine_with_other_bus(&mut self, other: &Bus) {
+        // This is the brute force search:
         let mut k0: i128 = 1;
         let mut k1: i128 = 1;
         loop {
@@ -139,97 +113,60 @@ impl Bus {
                 k0 += 1;
             } else {
                 // a0 > a1
+                // sometimes a0 >>> a1, so let's do a little math to jump by a bigger increment
+                // when possible.
                 let mut incr = (a0 - a1) / other.interval;
                 if incr < 1 { incr = 1; }
                 k1 += incr;
             };
         }
-        dbg!(k0, k1, self.arrival_time(k0), other.arrival_time(k1));
 
         // update bus:
-        self.launch = self.arrival_time(k0);
+        self.start_time = self.arrival_time(k0);
         self.interval = self.interval * other.interval;
     }
 }  // impl Bus
 
-// solve: k0 * id0 = k1 * id1 - offset
-//      n*k0*id0 = m * k1*id1 - offset
-//               = m * k1*id1 - offset
-//               
-fn pair_converge(id0: i128, id1: i128,
-                 offset: i128) -> (i128, i128) {
-    let mut k0: i128 = 1;
-    let mut k1: i128 = 1;
-    while (k0*id0 != k1*id1 - offset) {
-        if k0*id0 < k1*id1 - offset {
-            k0 += 1;
-        } else {
-            k1 += 1;
-        }
-    }
-    return (k0, k1);
-    // new bus looks likes:
-    //   interval of id0 * id1
-    //   offset 
-    //
-    // id0=3 id1=5
-    //  k0=3  k1=2
-    // original bus:
-    //    interval of id0
-    //    launch of 0
-    // new bus... id(01) = id0*id1 = 15
-    //    interval of id0*id1 = 15
-    //    launch of 9
-    //    x = 9 + n*15
-    // for example:
-    //    first intersect = 9, 10
-    //    second: 24, 25
-    //    third:  39, 40
-}
-
-// k1*i1 = k2*i2 - 1 = k3*i3 - 2 = k4*i4 - 3
-//
-// k2 = (k1*i1 + 1) / i2
-// n * k2 = (n*k1*i1 + n) / i2
-//
-
-/*
-fn converge_vector(ids: Vec<i128>, offsets: Vec<i128>) -> i128 {
-    let mut k: Vec<i128> = ids.iter().map(|_| 1).collect();
-    let mut incr = 1;
-    for i in 0..len(ids)-1 {
-        let j = i + 1;
-        let k = i + 2;
-        let (p1, p2) = converge_pairs(ids[i], ids[j], incr, 1);
-        let (n1, n2) = converge_pairs(ids[j], ids[k], incr, 1);
-        incr = 
-    }
-    return 0;
-}
-*/
-
-    
-/*
-fn find_lowest_common(text: &str) -> u128 {
-    // Part 2
-    // bus 0 departs at time x
-    // bus 1 departs at time x+1
-    // bus n departs at time x+n
-    //
-    // timestamp = k1*id1 = k2*id2 - 1 = k3*id3 - 2 = ...
-    // it turns out all bus ids are prime numbers.
-    //
-    // k1*id1 = k2*id2 - 1
-    // this pattern repeats id1*id2 units later, ie...
-    // (k1+n*id2)*id1 = (k2+n*id1)*id2 - 1  will be true for all integer n
-    //
-    // k1*id1 = k2*id2 - 1   *c
-    //   k1*id1*c = k2*id2*c - c
-    //
-    let bus_ids: Vec<i64> = text.split(",")
-        .map(|id| if *id == "x" { "1" } else { *id })
-        .map(|id| id.parse::<i64>().unwrap())
+fn part2(text: &str) -> i128 {
+    // Parse bus ids:
+    let v: Vec<_> = text.split(",")
+        .enumerate()
+        .filter(|(_, id)| *id != "x")
         .collect();
+    dbg!(&v);
 
+    // Construct bus ids to Bus objects, using index as the staggered start_time.
+    let buses: Vec<Bus> = v.iter()
+        .map(|(i, id)|
+             Bus { interval: id.parse::<i128>().unwrap(),
+                   start_time: (*i as i128) * -1, })
+        .collect();
+    dbg!(&buses);
+
+    // Combine all the buses into a super bus.
+    let mut bus: Bus = buses[0].clone();
+    for i in 1..buses.len() {
+        bus.combine_with_other_bus(&buses[i]);
+        dbg!(&bus);
+    }
+    return bus.start_time;  // Timestamp of first convergence.
 }
-*/
+
+fn main() {
+    let content = advent::load_input();
+
+    dbg!(part1(&content));
+
+    // brute force test of part2 solving
+    let v: Vec<i128> = vec![3i128, 5, 7, 11];
+    let o: Vec<i128> = vec![0i128, 1, 2, 3];
+    dbg!(bf_converge_vector(v, o));
+
+    // elegant faster solution over the same data:
+    dbg!(part2(&"3,5,7,11"));
+  
+    // the real solution to the input data set:
+    let lines: Vec<&str> = content.lines().collect();
+    dbg!("solution: ", part2(&lines[1]));
+}
+
