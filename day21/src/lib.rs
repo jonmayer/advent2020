d7fa28d26f4be80d57164586007a450c7780a7a1
Jonathan Mayer <jonathan.mayer@gmail.com>
2020-12-26 00:19:37 -0800

    Be helped me convert my Strings to str references.

--- day21/src/lib.rs
+++ day21/src/lib.rs
@@ -5,39 +5,39 @@ use std::collections::HashMap;
 use std::fs;
 
 #[derive(Debug,Default)]
-struct Food {
-    ingredients: HashSet<String>,
-    allergens: HashSet<String>,
+struct Food<'a> {
+    ingredients: HashSet<&'a str>,
+    allergens: HashSet<&'a str>,
 }
 
-impl Food {
-    fn new() -> Food {
+impl<'a> Food<'a> {
+    fn new() -> Food<'a> {
         Default::default()
     }
 
-    fn parse(&mut self, line: &str) {
+    fn parse(&mut self, line: &'a str) {
         let parts = line.split(" (contains ").take(2).collect::<Vec<&str>>();
-        self.ingredients.extend(parts[0].split_whitespace().map(|x| x.to_string()));
+        self.ingredients.extend(parts[0].split_whitespace());
         let mut allergens: &str = parts[1];
         allergens = &allergens[0..allergens.len()-1];
-        self.allergens.extend(allergens.split(", ").map(|x| x.to_string()));
+        self.allergens.extend(allergens.split(", "));
     }
 }
 
 #[derive(Default)]
-struct Foods {
-    foods: Vec<Food>,
-    all_allergens: HashSet<String>,
-    all_ingredients: HashSet<String>,
-    ingredient_count: HashMap<String, usize>,  // key is ingredient, value is counter
-    ingredients_map: HashMap<String, String>,  // key: ingredient, value: allergen
-    allergens_map: HashMap<String, String>,  // key: allergen, value: ingredient
+struct Foods<'a> {
+    foods: Vec<Food<'a>>,
+    all_allergens: HashSet<&'a str>,
+    all_ingredients: HashSet<&'a str>,
+    ingredient_count: HashMap<&'a str, usize>,  // key is ingredient, value is counter
+    ingredients_map: HashMap<&'a str, &'a str>,  // key: ingredient, value: allergen
+    allergens_map: HashMap<&'a str, &'a str>,  // key: allergen, value: ingredient
 }
 
-impl Foods {
-    fn new() -> Foods { Default::default() }
+impl<'a> Foods<'a> {
+    fn new() -> Foods<'a> { Default::default() }
 
-    fn parse(&mut self, text: &str) {
+    fn parse(&mut self, text: &'a str) {
         self.foods = text.lines()
             .map(|line| {
                 let mut food = Food::new();
@@ -69,20 +69,19 @@ impl Foods {
         for allergen in self.all_allergens.iter() {
             let mut possible_ingredients = self.all_ingredients.clone();
             // Remove known ingredients:
-            let known_ingredients: HashSet<String> = self.ingredients_map.keys()
-                .map(|x| x.clone()).collect();
-            possible_ingredients = possible_ingredients.difference(&known_ingredients).map(|x| x.clone()).collect();
+            let known_ingredients: HashSet<&'a str> = self.ingredients_map.keys().map(|x| *x).collect();
+            possible_ingredients = possible_ingredients.difference(&known_ingredients).map(|x| *x).collect();
             // Find the minimum set of foods that might contain this allergen:
             for food in self.foods.iter().filter(|f| f.allergens.contains(allergen)) {
                 possible_ingredients = possible_ingredients.intersection(&food.ingredients)
-                    .map(|x| x.clone()).collect();
+                    .map(|x| *x).collect();
             }
             dbg!(&possible_ingredients);
             // If only one ingredient could contain the allergen, add it to the map:
             if possible_ingredients.len() == 1 {
-                let ingredients = possible_ingredients.iter().map(|x| x.clone()).collect::<Vec<String>>();
-                self.ingredients_map.insert(ingredients[0].clone(), allergen.clone());
-                self.allergens_map.insert(allergen.clone(), ingredients[0].clone());
+                let ingredients = possible_ingredients.iter().map(|x| *x).collect::<Vec<&'a str>>();
+                self.ingredients_map.insert(ingredients[0], allergen);
+                self.allergens_map.insert(allergen, ingredients[0]);
                 println!("mapped ingredient {} to allergen {}", ingredients[0], allergen);
                 did_map = true;
             }
@@ -102,18 +101,18 @@ pub fn part1() -> usize {
     let mut foods = Foods::new();
     foods.parse(&contents);
     foods.deduce_all_ingredients();
-    let known_ingredients: HashSet<String> = foods.ingredients_map.keys()
-        .map(|x| x.clone()).collect();
-    let unknown_ingredients: HashSet<String> = foods.all_ingredients.difference(&known_ingredients)
-        .map(|x| x.clone()).collect();
+    let known_ingredients: HashSet<&str> = foods.ingredients_map.keys()
+        .map(|x| *x).collect();
+    let unknown_ingredients: HashSet<&str> = foods.all_ingredients.difference(&known_ingredients)
+        .map(|x| *x).collect();
     dbg!(unknown_ingredients.len());
     let result: usize = unknown_ingredients.iter()
         .map(|x| foods.ingredient_count[x])
         .sum();
     dbg!(result);
 
-    let mut sorted_allergens: Vec<String> = foods.allergens_map.keys()
-        .map(|x| x.clone()).collect();
+    let mut sorted_allergens: Vec<&str> = foods.allergens_map.keys()
+        .map(|x| *x).collect();
     sorted_allergens.sort();
     for allergen in sorted_allergens.iter() {
         print!("{},", foods.allergens_map[allergen]);
